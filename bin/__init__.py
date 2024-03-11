4f90fb1948260ad8ff697a1cb496267ed514ccc9
Enigma_Soul <119654171+Enigma-Soul@users.noreply.github.com>
2024-03-11 19:20:49 +0800

    Delete bin directory

--- bin/__init__.py
+++
@@ -1,6 +0,0 @@
-from .color import color
-from .clear import clear
-from .config import read,write
-from .getip import get_ip
-from .ranmac import ranmac
-from .help import help
--- bin/ranmac.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-def ranmac():
-    mac = [randint(0x00, 0xff) for _ in range(6)]
-    mac_address = ":".join(f"{x:02x}" for x in mac)
-    return mac_address
